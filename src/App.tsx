d30883335cad5a64b1211f8c1618fe6e4cfe2f04
kkarmazaite <karolinakarmazaite@gmail.com>
2026-08-12 14:14:11 +0300

    Adding React router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Listing from "./pages/Listing"
+import Favourites from "./pages/Favourites"
 
-function App() {
+export default function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+   <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/listing">Listing</Link>
+        <Link to="/favourites">Favourites</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listing" element={<Listing />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
